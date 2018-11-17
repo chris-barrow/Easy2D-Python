dfe48b201a582518633632f8a80e4bacb410b1fd
Christopher Barrow <christopher.barrow@uky.edu>
2018-11-17 13:38:00 -0500

    Continued refactor PREP, Rnode, and new Relem
    Continued refactoring PREP.py, reformatted Rnode.py to match PEP8 standards,
    and refactored and formatted Relem.py

--- PREP.py
+++ PREP.py
@@ -61,8 +61,8 @@ def PREP(inputfilename):
 
     outputFile.write('{}\n\n'.format(inputText[0]))
     nodes = np.zeros((NREC,6))
-    for i in range(4, NREC+3):
-        nodes[i-3,:] = np.fromstring(inputText[i], sep=',')
+    for i, node in enumerate(inputText[NODES_idx:NODES_idx+NREC+2]):
+        nodes[i,:] = np.fromstring(node, sep=',')
 
     N1=nodes[:,1]
     N2=nodes[:,2]
@@ -71,28 +71,32 @@ def PREP(inputfilename):
     X2=nodes[:,5]
     Y2=nodes[:,6]
     [X,Y]=Rnode(outputFile,NNODE,NREC,N1,N2,X1,Y1,X2,Y2)
-    elements=zeros(EREC,4)
-    for j=6+NREC:6+NREC+EREC-1
-        elements(j-5-NREC,:)=str2num(BLOCK{j})
 
-    NODEI=elements(:,1)
-    NODEL=elements(:,2)
-    NUMBER=elements(:,3)
-    KINDI=elements(:,4)
-    [NODE, KIND]=Relem(outputFile,NNODE,NELEM,EREC,NODEI,NODEL,NUMBER,KINDI)
-    bcs=zeros(BREC,6)
-    for k=8+NREC+EREC:8+NREC+EREC+BREC-1
-        bcs(k-7-NREC-EREC,:)=str2num(BLOCK{k})
+    elements=np.zeros((EREC,4))
+    for j, elem in enumerate(inputText[ELEM_idx+2:EREC+2]):
+        elements[j,:] = np.fromstring(elem, sep=',')
 
-    K1=bcs(:,1)
-    K2=bcs(:,2)
-    NOD=bcs(:,3)
-    CA1=bcs(:,4)
-    CB1=bcs(:,5)
-    CC1=bcs(:,6)
-    [TEMP,CA,CB,CC]= BC(outputFile, NNODE, NELEM, NODE, KIND, BREC,K1,K2,NOD,CA1,CB1,CC1)
-    if Field==2
-        FIELDS=zeros(FREC,2)
+    NODEI = elements[:,1]
+    NODEL = elements[:,2]
+    NUMBER = elements[:,3]
+    KINDI = elements[:,4]
+    [NODE, KIND] = Relem(outputFile, NNODE, NELEM, EREC, NODEI, NODEL, NUMBER,
+                         KINDI)
+
+    bcs = np.zeros((BREC,6))
+    for k, BC in enumerate(inputText[BC_idx + 2:BC_idx+BREC+1]):
+        bcs[k,:] = np.fromstring(BC, sep=',')
+
+    K1 = bcs[:, 1]
+    K2 = bcs[:,2]
+    NOD = bcs[:,3]
+    CA1 = bcs[:,4]
+    CB1 = bcs[:,5]
+    CC1 = bcs[:,6]
+    [TEMP, CA, CB, CC] = BC(outputFile, NNODE, NELEM, NODE, KIND, BREC, K1, K2,
+                            NOD, CA1, CB1, CC1)
+    if Field == 2:
+        FIELDS = np.zeros(FREC, 2)
         for l=10+NREC+EREC+BREC:10+NREC+EREC+BREC+FREC-1
             FIELDS(l-9-NREC-EREC-BREC,:)=str2num(BLOCK{l})
 
